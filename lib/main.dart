4286f0b311bc3ba9646523b72b3e9105e29d9161
rhernandezr <rhernandezr@jumapacelaya.gob.mx>
2022-02-23 15:55:15 -0600

    Terminado

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,45 @@ void main() async{
 
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final GlobalKey<NavigatorState> navigatorKey = new GlobalKey<NavigatorState>();
+  final GlobalKey<ScaffoldMessengerState> messengerKey = new GlobalKey<ScaffoldMessengerState>();
+
+  //Se convirtio a StateFulWiget para poder usar el init state
+  @override
+  void initState() {
+    super.initState();
+
+    //Ya se tiene acceso al context
+    PushNotificationService.getMessageStream.listen((message) {
+       print('**********************MyApp: $message');
+
+    //Cuando se recibe una notificación y se quiere navegar a otra pantalla (por como esta desrrollada esta app)
+    //No se puede usar el Navigator.pushNamed, porque a este nivel aún no se crea el context.
+    //Por eso se crean los GlobalKey
+
+      navigatorKey.currentState?.pushNamed('message',arguments: message);
+
+      final snackBar = SnackBar(content: Text(message));
+      messengerKey.currentState?.showSnackBar(snackBar);
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       initialRoute: 'home',
+      navigatorKey: navigatorKey,
+      scaffoldMessengerKey: messengerKey,
       routes: {
         'home' : (context) => HomeScreen(),
         'message': (context) => MessageScreen()
--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -1,6 +1,8 @@
 
 //SHA1: 54:8D:08:5B:8F:89:BB:AF:06:4D:7B:E4:0F:21:D3:03:D2:5F:16:DF
 
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
@@ -10,33 +12,57 @@ class PushNotificationService{
   //Obtner la instancia de Firebase que es parte de la configuración del archivo que instalamos
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
+  static StreamController<String> _messageStream = new StreamController.broadcast();
+
+  static Stream<String> get getMessageStream => _messageStream.stream;
 
   //Cuando la aplicación esta en la pila de apps
   static Future _onBackGroundHandler (RemoteMessage message) async{
-      print('onBackGroundHandker ${message.messageId}');
-  }
-  //Esta terminada la app pero puede escuchar
-  static Future _onMessageHandler (RemoteMessage message) async{
-      print('_onMessageHandler ${message.messageId}');
+      // print('onBackGroundHandler ${message.messageId}');
+      print(message.data);
+      // _messageStream.add(message.notification?.title?? 'No title');
+       _messageStream.add(message.data['producto']??'No Data');
   }
   //Esta abierta la app y el usuario la esta mirando
+  static Future _onMessageHandler (RemoteMessage message) async{
+      // print('_onMessageHandler ${message.messageId}');
+
+      print(message.data);
+      // _messageStream.add(message.notification?.body?? 'No title');
+      _messageStream.add(message.data['producto']??'No Data');
+  }
+  //Esta terminada la app pero puede escuchar
   static Future _onMessageOpenApp (RemoteMessage message) async{
-      print('_onMessageOpenApp ${message.messageId}');
+      // print('_onMessageOpenApp ${message.messageId}');
+      print(message.data);
+      // _messageStream.add(message.notification?.title?? 'No title');
+      _messageStream.add(message.data['producto']??'No Data');
+ 
+ 
   }
 
   static Future initializeApp() async{
     //Push Notifications
      //Para obtner esta información se instalo el paquete de firebase_core
-     await Firebase.initializeApp();
-     token = await FirebaseMessaging.instance.getToken();
+    await Firebase.initializeApp();
+    token = await FirebaseMessaging.instance.getToken();
+
+    print('token: $token');
 
     //Handlers
     FirebaseMessaging.onBackgroundMessage(_onBackGroundHandler);
+    //Esta abierta la app
     FirebaseMessaging.onMessage.listen(_onMessageHandler);
     FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
 
     //Local Notifications
 
+  }
+
+  static closeStreams(){
+    //Se pone esta linea porque se debe indicar cuando menos el código de cerrar el StreamController, en este
+    //tipo de caso nunca se va a cerrar, pero debe estar la línea de código.
+    _messageStream.close();
   }
 
 
